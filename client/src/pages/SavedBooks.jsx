d5bb5c2aa844d9b84cd12fa470b20b595dab8c98
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-22 18:19:08 -0400

    replace deleteBook with its graphql equivalent

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -6,12 +6,12 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
@@ -20,6 +20,9 @@ const SavedBooks = () => {
   // get the user's data
   const { data } = useQuery(GET_ME);
   const userData = data?.user || [];
+
+  // allow the user to delete a book
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -30,14 +33,8 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      await deleteBook(bookId, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
