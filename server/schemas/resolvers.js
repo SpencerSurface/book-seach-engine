9394e8e3b663cf527f7ba326d3d2a6a830b9228c
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-23 15:37:35 -0400

    fix error: typo in resolver name

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -44,7 +44,7 @@ const resolvers = {
             }
             throw AuthenticationError;
         },
-        deleteBook: async(parent, { bookId }, context) => {
+        removeBook: async(parent, { bookId }, context) => {
             if (context.user) {
                 return User.findOneAndUpdate(
                     { _id: context.user._id },
