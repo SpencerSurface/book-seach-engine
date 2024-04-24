0e1e4057f80cfdca50f4d26b4e9ed39ff859b54b
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-23 22:32:08 -0400

    add token middleware in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Navbar from './components/Navbar';
 
+// create the API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql"
+});
+
+// create the middleware to attach tokens to requests
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
+// create the Apollo client component
 const client = new ApolloClient({
-  uri: "/graphql",
+  // run the middleware then make the request to the API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
